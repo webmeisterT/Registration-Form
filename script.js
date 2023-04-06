5b51a33f9151cd7c9019cba105f7ebb431376b4d
webmeisterT <oluwatoyinakinlua@gmail.com>
2023-04-06 11:41:21 +0100

    updated

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 let firstname = document.getElementById('firstname')
 let middlename = document.getElementById('middlename')
-let lastname = document.getElementById('lastname').document.write()
+let lastname = document.getElementById('lastname')
 let email = document.getElementById('email')
 let passwd = document.getElementById('passwd')
 let passwd2 = document.getElementById('passwd2')
-let submit = document.getElementById('submit')
 let form = document.getElementById('form')
 
 form.addEventListener('submit', e => {
     e.preventDefault()
     let specialCharacters = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/
     // Trim and get values of the HTML element
+    let firstnameT = firstname.value.trim()
+    let middlenameT = middlename.value.trim()
+    let lastnameT = lastname.value.trim()
     let passwdT = passwd.value.trim()
     let passwd2T = passwd2.value.trim()
     let emailT = email.value.trim()
     
+    // check for emptiness
+    if (firstnameT.length === 0 || middlenameT.length === 0 || lastnameT.length === 0 || emailT.length ===0 || passwdT.length === 0) {
+        document.querySelector('#res').textContent = 'Please fill all fields!'
+        document.querySelector('#res').style.backgroundColor = 'red'
+        document.querySelector('#res').style.color = 'white'
+        document.querySelector('#res').style.padding = '8px'
+        return
+    }
+
     // check for email validity
     if (!emailT.split('').includes('@') || !emailT.split('').includes('.')) {
         email.style.borderStyle = "solid"
         email.style.borderColor = "red"
-        document.querySelector('#Erremail').textContent = "Your Email must be a valid one"
+        document.querySelector('#Erremail').textContent = "Email must be a valid one"
         return
     }
 
@@ -27,7 +38,7 @@ form.addEventListener('submit', e => {
     if (passwdT.length < 8) {
         passwd.style.borderStyle = "solid"
         passwd.style.borderColor = "red"
-        document.querySelector('#Errpasswd').textContent = "Your Password must be atleast 8 characters long"
+        document.querySelector('#Errpasswd').textContent = "Password must be atleast 8 characters long"
         return
     }
 
@@ -36,7 +47,7 @@ form.addEventListener('submit', e => {
         // passwdT.split('').some(el => console.log(typeof Number(el)));
         passwd.style.borderStyle = "solid"
         passwd.style.borderColor = "red"
-        document.querySelector('#Errpasswd').textContent = "Your Password must contain number"
+        document.querySelector('#Errpasswd').textContent = "Password must contain number"
         return
     }
 
@@ -44,19 +55,24 @@ form.addEventListener('submit', e => {
     if(!passwdT.split('').some(el => specialCharacters.test(el))) {
         passwd.style.borderStyle = "solid"
         passwd.style.borderColor = "red"
-        document.querySelector('#Errpasswd').textContent = "Your Password must contain special character"
+        document.querySelector('#Errpasswd').textContent = "Password must contain special character"
         return
     }
 
     // check the equality of password and confirm password
     if(passwdT != passwd2T) {
-        passwd.style.borderStyle = "solid"
+        passwd2.style.borderStyle = "solid"
         passwd2.style.borderColor = "red"
-        document.querySelector('#Errpasswd2').textContent = "Your Password must be thesame with confirm password"
+        document.querySelector('#Errpasswd2').textContent = "Password must be thesame as above"
         return
     }
 
     document.querySelector('#res').textContent = 'You have successfully registered'
     document.querySelector('#res').style.backgroundColor = 'green'
     document.querySelector('#res').style.color = 'white'
+    document.querySelector('#res').style.padding = '8px'
+
+    setTimeout(() => {        
+        location.reload()
+    }, 5000);
 })
